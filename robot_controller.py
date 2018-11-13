85abf7dcbf2c5cc415a3cd543d74896c532d8f1d
lynn2610 <lynn_mg_study@163.com>
2018-11-13 13:10:39 +0800

    update: 2018-11-13

--- robot_controller.py
+++ robot_controller.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2018/9/27 上午9:22
+# @Time    : 2018/9/27 9:22
 # @Author  : Lynn
 # @Site    : 
-# @File    : RobotControler.py
+# @File    : robot_controller.py
 # @Software: PyCharm
 
 import socket
@@ -13,9 +13,12 @@ import sys
 ############################################################################################
 
 class RobotController():
-    def __init__(self, pid = "192.168.39.220", port = 9876):
+    def __init__(self, pid="192.168.39.220", port=9876):
         """
         构造函数，设置对象的pid和port属性。
+
+        参数：
+        ----------
         :param pid:     机械臂IP地址
         :param port:    机械臂端口
         """
@@ -29,6 +32,9 @@ class RobotController():
     def send_OK(self):
         """
         向机械臂发送OK的确认信息。
+
+        返回值：
+        ----------
         :return:    None
         """
         print('SEND:OK')
@@ -43,36 +49,46 @@ class RobotController():
             sys.exit()
 
 
-    def move_car_init(self):
-        """
-        将机器人移动到自定义的初始位置[530, 0, 510, 180, 0, 0]
-        :return:        None
-        """
-        self.sk.sendall(bytes("1,530,0,510,180,0,0,\0", encoding='utf8'))
-
 
     def move_car(self, carPos):
         """
         通过笛卡尔坐标移动机械臂。
-        :param carPos:  笛卡尔坐标
-        :return:        None
+
+        参数：
+        ----------
+        :param carPos:  list
+                笛卡尔坐标([x,y,z,A,B,C], 精度：两位小数)
+        返回值：
+        ----------
+        :return:    None
         """
         strData = '1,{0},{1},{2},{3},{4},{5},\0'.format(carPos[0], carPos[1], carPos[2], carPos[3], carPos[4], carPos[5])
         self.sk.sendall(bytes(strData, encoding='utf8'))
 
 
 
-    def move_car_by_offset(self, offset_x = 0, offset_y = 0, offset_z = 0, offset_A = 0, offset_B = 0, offset_C = 0):
+    def move_car_by_offset(self, offset_x=0, offset_y=0, offset_z=0, offset_A=0, offset_B=0, offset_C=0):
         """
         传入笛卡尔坐标的偏移量并移动。
-        :param self: 
-        :param offset_x:    x轴偏移量
-        :param offset_y:    y轴偏移量
-        :param offset_z:    z轴偏移量
-        :param offset_A:    A的偏移量
-        :param offset_B:    B的偏移量
-        :param offset_C:    C的偏移量
-        :return:            None
+
+        参数：
+        ----------
+        :param offset_x: float
+                x轴偏移量(精度：两位小数)
+        :param offset_y: float
+                y轴偏移量(精度：两位小数)
+        :param offset_z: float
+                z轴偏移量(精度：两位小数)
+        :param offset_A: float
+                A的偏移量(精度：两位小数)
+        :param offset_B: float
+                B的偏移量(精度：两位小数)
+        :param offset_C: float
+                C的偏移量(精度：两位小数)
+
+        返回值：
+        ----------
+        :return:    None
         """
         strData = '3,{0},{1},{2},{3},{4},{5},\0'.format(offset_x, offset_y, offset_z, offset_A, offset_B, offset_C)
         # strDirection = '3,' + str(offset_x) + ',' + str(offset_y) + ',' + str(offset_z) + ',' + \
@@ -81,28 +97,47 @@ class RobotController():
 
 
 
-    def move_axis_by_offset(self, offset_a1 = 0, offset_a2 = 0, offset_a3 = 0, offset_a4 = 0, offset_a5 = 0, offset_a6 = 0):
+    def move_axis_by_offset(self, offset_a1=0, offset_a2=0, offset_a3=0, offset_a4=0, offset_a5=0, offset_a6=0):
         """
         传入关节坐标偏移量控制机械臂移动。
-        :param offset_a1:   关节a1
-        :param offset_a2:   关节a2
-        :param offset_a3:   关节a3
-        :param offset_a4:   关节a4
-        :param offset_a5:   关节a5
-        :param offset_a6:   关节a6
-        :return:            None
+
+        参数：
+        ----------
+        :param offset_a1: float
+                关节a1(精度：两位小数)
+        :param offset_a2: float
+                关节a2(精度：两位小数)
+        :param offset_a3: float
+                关节a3(精度：两位小数)
+        :param offset_a4: float
+                关节a4(精度：两位小数)
+        :param offset_a5: float
+                关节a5(精度：两位小数)
+        :param offset_a6: float
+                关节a6(精度：两位小数)
+
+        返回值：
+        ----------
+        :return:    None
         """
         strdata = '2,{0},{1},{2},{3},{4},{5},\0'.format(offset_a1,offset_a2,offset_a3,offset_a4,offset_a5,offset_a6)
-        self.sk.sendall(bytes(strdata, encoding = 'utf8'))
+        self.sk.sendall(bytes(strdata, encoding='utf8'))
 
 
 
     def move_axis(self, carPos, prea):
         """
         将笛卡尔坐标系转换成关节坐标系后，获取发送字符串。
-        :param carPos:  笛卡尔坐标
+
+        参数：
+        ----------
+        :param carPos: list
+                笛卡尔坐标([x,y,z,A,B,C], 精度：两位小数)
         :param prea:
-        :return:        None
+
+        返回值：
+        ----------
+        :return:    None
         """
         self.sk.sendall(bytes(self.car_to_axis_str(carPos, prea), encoding='utf8'))
 
@@ -111,14 +146,18 @@ class RobotController():
     def get_current_car_pos(self):
         '''
         向机械臂发送7，机械臂发送六个浮点数，即笛卡尔坐标值。
-        :return:    笛卡尔坐标值
+
+        返回值：
+        ----------
+        :return:  list
+                笛卡尔坐标值([x,y,z,A,B,C])
         '''
         print('SEND:7')
-        self.sk.sendall(bytes(str(7) + ',\0', encoding = 'utf8'))
+        self.sk.sendall(bytes(str(7) + ',\0', encoding='utf8'))
         carpos = []
         for i in range(6):
             data = self.sk.recv(7)
-            data.decode('utf-8')
+            data.decode('utf8')
             # print(data)
             data = str(data, encoding='utf8')
             # print(data)
@@ -127,16 +166,21 @@ class RobotController():
             # print(carpos)
 
         return carpos
+
 
 
     def get_current_axis_pos(self):
         '''
-        向机械臂发送6，机械臂发送六个浮点数，即关节坐标值。
-        :return:    关节坐标值
+        向机械臂发送6，机械臂回复发送六个浮点数，即关节坐标值。
+
+        返回值：
+        ----------
+        :return:  list
+                关节坐标值([a1,a2,a3,a4,a5,a6])
         '''
         print('SEND:6')
-        self.sk.sendall(bytes(str(6) + ',\0', encoding = 'utf8'))
-        carpos = []
+        self.sk.sendall(bytes(str(6) + ',\0', encoding='utf8'))
+        axispos = []
         for i in range(6):
             data = self.sk.recv(7)
             data.decode('utf-8')
@@ -144,19 +188,28 @@ class RobotController():
             data = str(data, encoding='utf8')
             # print(data)
             # carpos.append(round(float(data.split('\\')[0]), 2))
-            carpos.append(float(data))
+            axispos.append(float(data))
             # carpos.append(round(float(data), 2))
             # print(carpos)
 
-        return carpos
+        return axispos
 
 
 
     def car_to_axis_str(self, carPos, prea):
-        """调用sixAxesInverse将世界坐标系转换成关节坐标系，并返回发送字符串。
-            :param carPos:      世界坐标系
-            :param  prea:
-            :return :           返回值为关节坐标字符串
+        """
+        调用sixAxesInverse将世界坐标系转换成关节坐标系，并返回发送字符串。
+
+        参数：
+        ----------
+        :param carPos: list
+                笛卡尔坐标值([x,y,z,A,B,C], 精度：两位小数)
+        :param prea:
+
+        返回值：
+        ----------
+        :return: str
+                关节坐标字符串
         """
         inv = sai.Inverse(carPos[0], carPos[1], carPos[2], carPos[3], carPos[4], carPos[5], prea)
         solution, iloc = inv.inverse()
@@ -168,21 +221,48 @@ class RobotController():
 
 
 
-    def control_paw(self, flag = 4):
+    def control_paw(self, flag=4, pulse_cnt=300):
         """
         控制机器人手爪的闭合与张开。
+
+        参数：
+        ----------
         :param flag:    4-张开手爪， 5-闭合手爪
+
+        返回值：
+        ----------
         :return:        None
         """
-        # print('SEND:4')
-        strSend = str(flag) + ',\0'
+        strSend = str(flag) + ',' + str(pulse_cnt) + ',\0'
         self.sk.sendall(bytes(strSend, encoding='utf8'))
+
+
+
+    def set_speed(self, rate=10):
+        '''
+        控制机器人运动的速度，速度为原速度的 speed_rate/10。
+
+        参数：
+        ----------
+        :param speed_rate: int
+                速度分为10个等级，即设置速度为原速度的speed_rate/10。
+
+        返回值：
+        ----------
+        :return:        None
+        '''
+        strSend = str(8) + ',' + str(rate) + ',\0'
+        self.sk.sendall(bytes(strSend, encoding='utf8'))
+
 
 
 
     def close(self):
         """
         断开和机械臂的连接。
+
+        返回值：
+        ----------
         :return:        None
         """
         self.sk.close()
---
+++ test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/8 15:51
+# @Author  : Lynn
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+"""
+拍摄高度：510
+抓取高度：167.15
+x:[330,530]
+y:[-118,118]
+z:[167.15,510]
+(530,0,510)
+"""
+"""
+green:(513.97,-35.96,231.69)
+orange:(524.35,56.18,188.45)
+camera:3.7cm
+"""
+
+from robot_controller import *
+import time
+import numpy as np
+import os
+
+def delete_image(delDir = '.'):
+    # delList = []
+    # delLis = os.listdir(delDir)
+    for root, dirs, files in os.walk(delDir):
+        for name in files:
+            if(name.endswith('.jpg')):
+                os.remove(os.path.join(root, name))
+
+def main():
+    # delete_image()
+    # tlen = 128
+    init_pos = [400, 100, 510, 180, 0, 0]
+    test_pos = [500, -200, 360, 180, 0, 0]
+    # test_pos = [510, 260, 175, 180, 0, 0]
+    # init_pos_str = "1,530,0,390,180,0,0"
+    # pos_tmp = [507.83, 92.01, round(359.32 - tlen, 2), 180, 0, 0]
+    # pos_tmp = [548.97 + 41, 1, 169, 180, 0, 0]
+    prea = np.array([0, 56, -17, 0, 24, 0])
+
+    robot_instance = RobotController()
+    robot_instance.send_OK()
+    # robot_instance.move_car_by_offset(offset_C = 180)
+    # robot_instance.move_axis_by_offset(offset_a2 = 30)
+
+    # for i in range(3):
+    #     robot_instance.control_paw(5, 100)
+    #     time.sleep(1)
+
+    # robot_instance.control_paw(5)
+
+    # robot_instance.control_paw(4)
+    robot_instance.set_speed(1)
+    time.sleep(1)
+    # while True:
+    robot_instance.move_car(test_pos)
+    time.sleep(2)
+    robot_instance.move_car(init_pos)
+        # time.sleep(2)
+        # robot_instance.set_speed(4)
+        # time.sleep(1)
+    # print(robot_instance.get_current_car_pos())
+    # print(robot_instance.get_current_axis_pos())
+    # time.sleep(3)
+    # robot_instance.move_car_by_offset(offset_z=-10)
+    # robot_instance.move_car_by_offset(offset_x=-10)
+    # print(robot_instance.get_current_axis_pos())
+    # print(robot_instance.get_current_car_pos())
+    robot_instance.close()
+
+if __name__ == '__main__':
+    main()
